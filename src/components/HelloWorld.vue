d6d5419104af34e199fcea02a7c2e63567e237ac
Gilson Doi Junior <doijunior@yahoo.com.br>
2018-08-31 17:40:00 -0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="hello">
+    <div v-if="marker">
+      Latitude: {{this.lat}},
+      Longitude: {{this.lng}}
+      <label>Árvore:</label>
+      <input type='text' v-model='mainTag'/>
+      <label>Descrição complementar:</label>
+      <input type='text' v-model='description'/>
+      <button v-on:click='save'>Adicionar</button>
+    </div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import leaflet from 'leaflet';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      info: null,
+      mainTag: "",
+      description: "",
+      marker: undefined
+    }
+  },
+  mounted () {
+    var mymap = L.map('map');
+    mymap.setView([-23.31455,-51.17181], 14);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    var newMarker;
+    let self = this;
+
+    mymap.on('click', function(e) {
+      if(newMarker)
+        mymap.removeLayer(newMarker);
+      newMarker = L.marker(e.latlng).addTo(mymap);
+      console.log("antes    "+self.marker);
+      self.marker = newMarker;
+      console.log(self.marker);
+    });
+
+    axios
+      .get('http://ciburbem.firebaseapp.com/trees')
+      .then(response => {
+        let trees = response.data;
+
+        for(let id in trees){
+          let tree = trees[id];
+          var greenIcon = L.icon({
+              iconUrl: 'icon.png',
+
+              iconSize:     [64, 64], // size of the icon
+              iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+              shadowAnchor: [4, 62],  // the same for the shadow
+              popupAnchor:  [13, -76] // point from which the popup should open relative to the iconAnchor
+          });
+          L.marker([tree.lat, tree.lng], {icon: greenIcon}).addTo(mymap).bindPopup("<p>"+tree.mainTag+"</p>"+tree.description);
+        }
+      }).catch(error => {
+        console.log(error);
+      })
+  },
+  computed: {
+    lat: {
+      get: function(){
+        if(this.marker)
+          return this.marker.getLatLng().lat;
+        else {
+          return "";
+        }
+      }
+    },
+    lng: {
+      get: function(){
+        if(this.marker)
+          return this.marker.getLatLng().lng;
+        else {
+          return "";
+        }
+      }
+    },
+  },
+  methods: {
+    save: function (event) {
+      console.log("OI MUNDO");
+      console.log("depois:"+this.marker+this.mainTag+this.description);
+      // const provider = new OpenStreetMapProvider();
+      // const results = await provider.search({ query: this.$data.address });
+      // console.log(results);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+#map
+  width: 100%
+  height: 400px
+  font-weight: bold
+  font-size: 13px
+  text-shadow: 0 0 2px #fff
+  .leaflet-shadow-pane > .leaflet-marker-shadow
+    display: none
+h3
+  margin: 40px 0 0
+
+ul
+  list-style-type: none
+  padding: 0
+li
+  display: inline-block
+  margin: 0 10px
+a
+  color: #42b983
+</style>

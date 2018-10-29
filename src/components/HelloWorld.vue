4a68b7077b176812d95b28ef0e59cde04c59eca1
Gilson <doijunior@yahoo.com.br>
2018-10-29 17:00:07 -0300

    loading markers from google drive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,47 +27,58 @@ export default {
       info: null,
       mainTag: "",
       description: "",
-      marker: undefined
+      marker: undefined,
+      map: undefined
     }
   },
   mounted () {
-    var mymap = L.map('map');
-    mymap.setView([-23.31455,-51.17181], 14);
+    this.map = L.map('map');
+    this.map .setView([-23.31455,-51.17181], 14);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
+    }).addTo(this.map );
     var newMarker;
     let self = this;
 
-    mymap.on('click', function(e) {
+    this.map.on('click', function(e) {
       if(newMarker)
-        mymap.removeLayer(newMarker);
-      newMarker = L.marker(e.latlng).addTo(mymap);
+        self.map.removeLayer(newMarker);
+      newMarker = L.marker(e.latlng).addTo(self.map);
       console.log("antes    "+self.marker);
       self.marker = newMarker;
       console.log(self.marker);
     });
-
     axios
-      .get('http://ciburbem.firebaseapp.com/trees')
+      .get('https://spreadsheets.google.com/feeds/list/1bFy086G8dGg6tzK4fNyI8I6LKXWuc9gjXlXNXxJDRdY/od6/public/basic?alt=json')
       .then(response => {
-        let trees = response.data;
+        self.points = (response.data.feed.entry.map((item) => {
+          let mark = item.content["$t"].replace(/\w+:/g, "").split(',');
+          console.log([Number(mark[0]), Number(mark[1])]);
+          let marker = L.marker([Number(mark[0]), Number(mark[1])]).addTo(self.map).bindPopup(mark[2]);
+          return mark;
+        }));
+      });
 
-        for(let id in trees){
-          let tree = trees[id];
-          var greenIcon = L.icon({
-              iconUrl: 'icon.png',
-
-              iconSize:     [64, 64], // size of the icon
-              iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-              shadowAnchor: [4, 62],  // the same for the shadow
-              popupAnchor:  [13, -76] // point from which the popup should open relative to the iconAnchor
-          });
-          L.marker([tree.lat, tree.lng], {icon: greenIcon}).addTo(mymap).bindPopup("<p>"+tree.mainTag+"</p>"+tree.description);
-        }
-      }).catch(error => {
-        console.log(error);
-      })
+    // axios
+    //   .get('http://ciburbem.firebaseapp.com/trees')
+    //   .then(response => {
+    //     let trees = response.data;
+    //
+    //     for(let id in trees){
+    //       let tree = trees[id];
+    //       var greenIcon = L.icon({
+    //           iconUrl: 'icon.png',
+    //
+    //           iconSize:     [64, 64], // size of the icon
+    //           iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    //           shadowAnchor: [4, 62],  // the same for the shadow
+    //           popupAnchor:  [13, -76] // point from which the popup should open relative to the iconAnchor
+    //       });
+    //       L.marker([tree.lat, tree.lng], {icon: greenIcon}).addTo(mymap).bindPopup("<p>"+tree.mainTag+"</p>"+tree.description);
+    //     }
+    //   }).catch(error => {
+    //     console.log(error);
+    //   })
   },
   computed: {
     lat: {

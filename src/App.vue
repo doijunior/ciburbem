29de272705618c68c95e602155eec9b9600f03b8
Gilson <doijunior@yahoo.com.br>
2018-11-07 17:54:33 -0200

    initial vuetify layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+  <v-app>
+    <v-toolbar app>Ciburbem Londrina</v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <v-form>
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            label="Name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -11,6 +33,12 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      name: "",
+      email: ""
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import 'vuetify/dist/vuetify.min.css';
+
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App)

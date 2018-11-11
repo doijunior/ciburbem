a998f86324cb8a08e90e87ef0d27ff041a141bdc
Gilson Doi Junior <doijunior@yahoo.com.br>
2018-11-11 20:51:18 -0200

    layout completo v1

--- src/components/Home/Form.vue
+++ src/components/Home/Form.vue
@@ -1,25 +1,55 @@
 <template>
   <v-form ref="form">
-    <v-layout row>
-      <v-autocomplete
-        v-model="tree"
-        :items="treeOptions"
-        :label="'Árvore'"
-        prepend-icon="fas fa-search"
+    <v-autocomplete
+      v-model="treeFilter"
+      :items="treeOptions"
+      :label="'Árvore'"
+      prepend-icon="fas fa-search"
+      @change="teste"
+    >
+      <v-slide-x-reverse-transition
+        slot="append-outer"
+        mode="out-in"
       >
-        <v-slide-x-reverse-transition
-          slot="append-outer"
-          mode="out-in"
-        >
-        </v-slide-x-reverse-transition>
-      </v-autocomplete>
-      <v-text-field
-        v-model="description"
-        label="Descrição"
-        required
-      ></v-text-field>
-      <v-btn color="success">Salvar</v-btn>
-    </v-layout>
+      </v-slide-x-reverse-transition>
+    </v-autocomplete>
+
+    <v-expansion-panel>
+      <v-expansion-panel-content>
+        <div slot="header">Cadastrar Ponto</div>
+        <v-card>
+          <v-layout row>
+            <v-autocomplete
+              v-model="tree"
+              :items="treeOptions"
+              :label="'Árvore'"
+              v-if="!other"
+            >
+              <v-slide-x-reverse-transition
+                slot="append-outer"
+                mode="out-in"
+              >
+              </v-slide-x-reverse-transition>
+            </v-autocomplete>
+            <v-text-field
+              v-model="tree"
+              label="Árvore"
+              v-if="other"
+            ></v-text-field>
+            <v-checkbox
+              v-model="other"
+              label="Outra"
+            ></v-checkbox>
+            <v-text-field
+              v-model="description"
+              label="Descrição"
+              required
+            ></v-text-field>
+            <v-btn color="success">Salvar</v-btn>
+          </v-layout>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
   </v-form>
 </template>
 
@@ -36,10 +66,17 @@ export default {
   data(){
     return {
       tree: "",
+      treeFilter: "",
       description: "",
       lat: undefined,
-      lng: undefined
+      lng: undefined,
+      other: false
     }
   },
+  methods: {
+    teste(){
+      console.log("OI");
+    }
+  }
 }
 </script>
